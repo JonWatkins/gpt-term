3b4159872b9ea41770eb8bd6b420689dcf6c8ad4
Jon Watkins <jonwatkins86@gmail.com>
2023-08-05 02:31:47 +0100

    Added a loading spinner

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { program } from "commander";
 import chalk from "chalk";
+import ora from "ora";
 import { getPrompt, getResponse, ChatMessage } from "./utils";
 
 program.version("0.0.6");
@@ -9,10 +10,15 @@ program
   .option("-m, --model <model>", "ChatGpt model to use", "gpt-3.5-turbo")
   .option("-v, --verbose", "Run with verbose logging")
   .action(async (opts) => {
+    if (opts.verbose) {
+      console.info(chalk.green(`Starting new chat using ${opts.model}`));
+    }
+
     const messages: ChatMessage[] = [];
 
     const processNextMessage = async (): Promise<void> => {
       const prompt = await getPrompt();
+      const spinner = ora("ChatGPT: Thinking...");
 
       if (["exit", "quit", "bye"].includes(prompt)) {
         console.log(chalk.green("goodbye"));
@@ -21,9 +27,11 @@ program
 
       const message: ChatMessage = { role: "user", content: prompt };
       messages.push(message);
+      spinner.start();
 
       try {
         const response = await getResponse(messages, opts.model);
+        spinner.succeed();
         if (response) {
           messages.push(response as ChatMessage);
           console.log("ChatGPT: ", response?.content);
